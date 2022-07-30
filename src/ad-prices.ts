6653f83e4b8af861d3516d00e9e01da3b518f1e7
Éverton M. Vieira <emuvi@outlook.com.br>
2022-07-30 17:06:39 -0300

    adds the prices register

---
+++ src/ad-prices.ts
@@ -0,0 +1,43 @@
+import {
+  AdExpect,
+  AdFilter,
+  AdModule,
+  AdModules,
+  AdRegBase,
+  AdRegister,
+  AdRegistry,
+  AdTools,
+} from "admister";
+import { QinTool } from "qin_case";
+import { registry as products_regy } from "./ad-products";
+
+const base = QinTool.qinpel.chief.loadConfig(QinTool.qinpel.our.names.QinBaseSelected);
+
+export const registry: AdRegistry = { base, name: "precos" };
+
+export const register: AdRegBase = {
+  registry,
+  joins: [
+    {
+      module: AdModules.PRODUCTS,
+      registry: products_regy,
+      alias: "products",
+      filters: [
+        new AdFilter({
+          linked: { name: "produto", with: "codigo" },
+        }),
+      ],
+    },
+  ],
+};
+
+export class AdPrices extends AdRegister {
+  public constructor(module: AdModule, expect: AdExpect) {
+    super(module, expect, register);
+    this.addField(AdTools.newAdFieldString("produto", "Produto - Cód.", 6).putKey());
+    this.addField(AdTools.newAdFieldString("products.nome", "Produto - Nome.", 60));
+    this.addField(AdTools.newAdFieldString("tabela", "Tabela", 6).putKey());
+    this.addField(AdTools.newAdFieldNumeric("valor", "Valor"));
+    this.prepare();
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { AdMenuItem, AdModules, menuStartUp } from "admister";
+import { AdPrices } from "./ad-prices";
 import { AdProducts } from "./ad-products";
 import { AdProductsGroup } from "./ad-products-group";
 import { AdProductsSubGroup } from "./ad-products-subgroup";
@@ -7,6 +8,7 @@ const items: AdMenuItem[] = [
   { module: AdModules.PRODUCTS, register: AdProducts },
   { module: AdModules.PRODUCTS_GROUP, register: AdProductsGroup },
   { module: AdModules.PRODUCTS_SUBGROUP, register: AdProductsSubGroup },
+  { module: AdModules.PRICES, register: AdPrices },
 ];
 
 menuStartUp(items).style.putAsBody();
