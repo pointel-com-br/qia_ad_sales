5e7221cc0272f5ccfedf38fc3f71a7f293b1914e
Éverton M. Vieira <emuvi@outlook.com.br>
2022-09-16 18:30:41 -0300

    adds payment terms to the sales

--- src/ad-sales.ts
+++ src/ad-sales.ts
@@ -23,6 +23,11 @@ export const regBased: AdRegBased = {
       alias: "clients",
       filters: [{ linked: { name: "cliente", with: "codigo" } }],
     },
+    {
+      module: AdModules.PAYMENT_TERMS,
+      alias: "payment_terms",
+      filters: [{ linked: { name: "cond_pagamento", with: "codigo" } }],
+    },
   ],
 };
 
@@ -32,6 +37,8 @@ export class AdSales extends AdRegister {
     this.addField(AdTools.newAdFieldString("codigo", "Código", 10).putKey());
     this.addField(AdTools.newAdFieldString("cliente", "Cliente - Cod.", 8));
     this.addField(AdTools.newAdFieldString("clients.nome", "Cliente - Nome.", 60));
+    this.addField(AdTools.newAdFieldString("cond_pagamento", "Cond. Pgto - Cod.", 8));
+    this.addField(AdTools.newAdFieldString("payment_terms.nome", "Cond. Pgto - Nome.", 60));
     this.prepare();
   }
 }
