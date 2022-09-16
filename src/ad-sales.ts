1d6fe97f4d5757edc9962fbeb6539eb74e9edf7d
Éverton M. Vieira <emuvi@outlook.com.br>
2022-09-16 19:27:55 -0300

    sets read only fields immutable on sales

--- src/ad-sales.ts
+++ src/ad-sales.ts
@@ -35,17 +35,17 @@ export class AdSales extends AdRegister {
   public constructor(module: AdModule, expect: AdExpect) {
     super(module, expect, regBased);
     this.addField(AdTools.newAdFieldString("codigo", "Código", 10).putKey());
-    this.addField(AdTools.newAdFieldDate("emitido_data", "Emitido Em"));
-    this.addField(AdTools.newAdFieldBoolean("enviado", "Enviado"));
-    this.addField(AdTools.newAdFieldDate("enviado_data", "Enviado Em"));
+    this.addField(AdTools.newAdFieldDate("emitido_data", "Emitido Em").putReadOnly());
+    this.addField(AdTools.newAdFieldBoolean("enviado", "Enviado").putReadOnly());
+    this.addField(AdTools.newAdFieldDate("enviado_data", "Enviado Em").putReadOnly());
     this.addField(AdTools.newAdFieldString("cliente", "Cliente - Cod.", 8));
     this.addField(AdTools.newAdFieldString("clients.nome", "Cliente - Nome.", 60));
     this.addField(AdTools.newAdFieldNumeric("out_desc", "Out. Descontos"));
     this.addField(AdTools.newAdFieldNumeric("out_acresc", "Out. Acréscimos"));
-    this.addField(AdTools.newAdFieldNumeric("subtotal", "SubTotal"));
-    this.addField(AdTools.newAdFieldNumeric("desc_itens", "Desc. Itens"));
-    this.addField(AdTools.newAdFieldNumeric("acresc_itens", "Acresc. Itens"));
-    this.addField(AdTools.newAdFieldNumeric("total", "Total"));
+    this.addField(AdTools.newAdFieldNumeric("subtotal", "SubTotal").putReadOnly());
+    this.addField(AdTools.newAdFieldNumeric("desc_itens", "Desc. Itens").putReadOnly());
+    this.addField(AdTools.newAdFieldNumeric("acresc_itens", "Acresc. Itens").putReadOnly());
+    this.addField(AdTools.newAdFieldNumeric("total", "Total").putReadOnly());
     this.addField(AdTools.newAdFieldString("cond_pagamento", "Cond. Pgto - Cod.", 4));
     this.addField(AdTools.newAdFieldString("payment_terms.nome", "Cond. Pgto - Nome.", 45));
     this.addField(AdTools.newAdFieldString("obs", "Obs", 400));
