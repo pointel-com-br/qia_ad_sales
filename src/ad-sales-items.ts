49aedd22bcef87bbd845b14b1ae274b5910fd086
Éverton M. Vieira <emuvi@outlook.com.br>
2022-10-20 21:18:49 -0300

    implements the values updates on item prices

--- src/ad-sales-items.ts
+++ src/ad-sales-items.ts
@@ -17,23 +17,64 @@ export const registier: AdRegistier = { base, registry };
 
 export const regBased: AdRegBased = {
   registier,
+  joins: [
+    {
+      module: AdModules.PRODUCTS,
+      alias: "products",
+      filters: [{ linked: { name: "produto", with: "codigo" } }],
+    },
+  ],
 };
 
 export class AdSalesItems extends AdRegister {
+  private _updateValues = (_: any) => {
+    let quantidade = this.model.getFieldByName("quantidade").value;
+    if (!quantidade) quantidade = 0.0;
+    let preco = this.model.getFieldByName("preco").value;
+    if (!preco) preco = 0.0;
+    let subtotal = quantidade * preco;
+    this.model.getFieldByName("subtotal").value = subtotal;
+    let desconto = 0.0;
+    let desconto_per = this.model.getFieldByName("desconto_per").value;
+    if (!desconto_per) desconto_per = 0.0;
+    if (desconto_per > 0.0) {
+      desconto = (subtotal * desconto_per) / 100.0;
+    }
+    this.model.getFieldByName("desconto").value = desconto;
+    let acrescimo = 0.0;
+    let acrescimo_per = this.model.getFieldByName("acrescimo_per").value;
+    if (!acrescimo_per) acrescimo_per = 0.0;
+    if (acrescimo_per > 0.0) {
+      acrescimo = (subtotal * acrescimo_per) / 100.0;
+    }
+    this.model.getFieldByName("acrescimo").value = acrescimo;
+    let total = subtotal + acrescimo - desconto;
+    this.model.getFieldByName("total").value = total;
+  };
+
   public constructor(module: AdModule, expect: AdExpect) {
     super(module, expect, regBased);
-    this.addField(AdTools.newAdFieldString("prepedido", "PréPedido", 10).putKey());
+    this.addField(
+      AdTools.newAdFieldString("prepedido", "PréPedido", 10).putKey().putReadOnly()
+    );
     this.addField(AdTools.newAdFieldString("codigo", "Código", 4).putKey());
     this.addField(AdTools.newAdFieldString("produto", "Produto - Cód.", 4));
-    this.addField(AdTools.newAdFieldNumeric("quantidade", "Quantidade"));
+    this.addField(AdTools.newAdFieldString("products.nome", "Produto - Nome.", 60));
+    this.addField(
+      AdTools.newAdFieldNumeric("quantidade", "Quantidade").putOnChanged(this._updateValues)
+    );
     this.addField(AdTools.newAdFieldString("tabela", "Tabela", 6));
-    this.addField(AdTools.newAdFieldNumeric("preco", "Preço"));
-    this.addField(AdTools.newAdFieldNumeric("subtotal", "SubTotal"));
-    this.addField(AdTools.newAdFieldNumeric("desconto_per", "% Desconto"));
-    this.addField(AdTools.newAdFieldNumeric("desconto", "Desconto"));
-    this.addField(AdTools.newAdFieldNumeric("acrescimo_per", "% Acréscimo"));
-    this.addField(AdTools.newAdFieldNumeric("acrescimo", "Acréscimo"));
-    this.addField(AdTools.newAdFieldNumeric("total", "Total"));
+    this.addField(AdTools.newAdFieldNumeric("preco", "Preço").putOnChanged(this._updateValues));
+    this.addField(AdTools.newAdFieldNumeric("subtotal", "SubTotal").putReadOnly());
+    this.addField(
+      AdTools.newAdFieldNumeric("desconto_per", "% Desconto").putOnChanged(this._updateValues)
+    );
+    this.addField(AdTools.newAdFieldNumeric("desconto", "Desconto").putReadOnly());
+    this.addField(
+      AdTools.newAdFieldNumeric("acrescimo_per", "% Acréscimo").putOnChanged(this._updateValues)
+    );
+    this.addField(AdTools.newAdFieldNumeric("acrescimo", "Acréscimo").putReadOnly());
+    this.addField(AdTools.newAdFieldNumeric("total", "Total").putReadOnly());
     this.addField(AdTools.newAdFieldString("obs", "Obs", 100));
     this.prepare();
   }
